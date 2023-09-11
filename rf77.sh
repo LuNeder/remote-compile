5e3e20c9d244e4b263c38462417e23ad2be7d9c7
Luana <luana@luana.dev.br>
2023-09-11 18:01:47 -0300

    Update rf77.sh

--- rf77.sh
+++ rf77.sh
@@ -21,9 +21,22 @@
 case $1 in
     --install)
         echo 'Instalando o Remote f77'
-        echo 'Qual o usuário de ssh? (Exemplo (se seu num usp for 1234): a1234@basalto.ifsc.usp.br)'
-        read USER
-        mkdir ~/.config
-        echo $
+        echo 'Qual o usuário completo do ssh? (Exemplo (se seu num usp for 1234 digite o seguinte e de enter): a1234@basalto.ifsc.usp.br)'
+        read rf77USER
+        mkdir ~/.config/remotef77
+        echo $rf77USER > ~/.config/remotef77/user
+        echo 'Deseja salvar a senha do Basalto num arquivo? Se sim, digite a senha que você usa para conectar por ssh agora. Se não quiser, deixe em branco e de enter - nesse caso o programa irá perguntar sua senha toda vez que for rodado.'
+        read rf77PASSWD
+        if [ "x$rf77PASSWD" != "x" ]; then 
+            echo $rf77PASSWD > ~/.config/remotef77/passwd
+            echo 'Senha salva'
+        fi
+        echo 'Deseja instalar o rf77 em sua PATH, para poder utilizar o programa simplesmente ao rodar "rf77"?'
+        echo 'Para instalar, tenha certeza que você rodou o rf77 --install como root("sudo '$0 $1'")' 
+        echo 'Se você responder que não quer instalar, poderá usar o rf77 rodando "'$0
+        echo 'Responda com "sim" ou "nao" (sem as aspas) e de enter.' 
+
+ 
+
 
 esac
